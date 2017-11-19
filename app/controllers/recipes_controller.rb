262291f353c6013ca1f10e0a8a931c26d9405618
Sophia Varella <sophiavarella@gmail.com>
2017-11-19 12:08:41 -0800

    create recipe show action and view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,17 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+  
   def index
     @recipes = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end
