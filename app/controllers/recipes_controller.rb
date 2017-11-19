4a866fb7323303997c771001068ba7907f1cfd74
Sophia Varella <sophiavarella@gmail.com>
2017-11-19 14:04:19 -0800

    Add the create recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,24 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      redirect_to recipe_path(recipe)
+    else
+      render :new
+    end
+  end
+
   private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :category, :description, :prep_time)
+    end
 
     def set_recipe
       @recipe = Recipe.find(params[:id])
