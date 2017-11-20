4743c76655d1e5b7bcc5c7bab01d144bb55fe9a3
Sophia Varella <sophiavarella@gmail.com>
2017-11-19 16:05:45 -0800

    Add validations for Recipe and Ingredient models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+  validates :name, presence: true
+  validates :quantity, numericality: { only_integer: true }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
+  validates :title, :description, :category, presence: true
+  validates :title, length: { in: 1..30 }
+
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? }
 end
